e1bb6fa6f01f7311b9aa577632ce326edbe43bee
endaaman <buhibuhidog@gmail.com>
2014-03-25 17:52:26 +0900

    merged

--- ajax/views.py
+++ ajax/views.py
@@ -4,11 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
-=======
-# from django.views.decorators.csrf import csrf_protect
-
-
->>>>>>> tmp
 def mail(request):
     if request.method == 'POST':
         return http.HttpResponse('frate1839@gmail.com', mimetype='text/plain')
