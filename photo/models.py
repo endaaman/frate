4e7c8aae6b9537404275f4748db1bd003283392d
endaaman <buhibuhidog@gmail.com>
2014-04-30 23:01:49 +0900

    tmpさぎょうちう

--- photo/models.py
+++ photo/models.py
@@ -12,9 +12,6 @@ from PIL import Image
 import os
 
 
-fs = FileSystemStorage()
-
-
 class Album(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="投稿日時")
     title = models.CharField(max_length=200, blank=False, verbose_name="タイトル")
@@ -36,7 +33,9 @@ class Photo(models.Model):
     author = models.ForeignKey(Member, related_name='author')
     message = models.TextField(blank=False, verbose_name="本文")
     member = models.ManyToManyField(Member, blank=True, related_name='member')
-    image = ImageWithThumbsField(upload_to='photo', storage=fs, sizes=((200, 200),), verbose_name="写真")
+    image = models.ImageField(upload_to='photo', verbose_name="写真")
+    thumb = models.ImageField(upload_to='photo', editable=False, verbose_name="サムネイル")
+
 
     class Meta:
         verbose_name = verbose_name_plural = '写真'
@@ -47,7 +46,8 @@ class Photo(models.Model):
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
 
-        if self.image and not os.path.exists(self.image.path):
+        # if self.image and not os.path.exists(self.image.path):
+        if self.image:
             from cStringIO import StringIO
 
             self.image.open('r+b')
@@ -63,9 +63,10 @@ class Photo(models.Model):
                 s = im.size[0]
                 l = im.size[1]
 
-            if l > 1920:
-                a = 1920 * s / l
-                im.thumbnail(y and (1920, a) or (a, 1920), Image.ANTIALIAS)
+            new_size = 1920
+            if l > new_size:
+                a = new_size * s / l
+                im.thumbnail(y and (new_size, a) or (a, new_size), Image.ANTIALIAS)
                 print 'resize'
 
             fp = StringIO()
@@ -77,6 +78,24 @@ class Photo(models.Model):
 
             # self.image.close()
             fp.close()
+
+        root, ext = os.path.splitext(self.image.path)
+
+        thumb_filename = '%s-thumb%s' % (root, ext)
+
+        y = im.size[0] > im.size[1]
+        if y:
+            l = im.size[0]
+            s = im.size[1]
+        else:
+            s = im.size[0]
+            l = im.size[1]
+
+        thumb_size = 200
+
+        if l > thumb_size:
+            a = thumb_size * l / s
+            im.resize(y and (a, thumb_size) or (thumb_size, a), Image.ANTIALIAS).save(thumb_filename)
 
         r = super(Photo, self).save(force_insert, force_update, using, update_fields)
         return r
