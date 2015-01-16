d3a9c00c806772d129c0f3fd7b17c36f21b53393
endaaman <buhibuhidog@gmail.com>
2015-01-17 03:11:48 +0900

    commit

--- ,/other/snippet.py
+++
@@ -1,9 +0,0 @@
-from bbs.models import *
-from bbs.views import *
-c = Comment.objects.all()
-df = DeleteForm(dict(edit_key='13'), instance=c[0])
-df.is_valid()
-
-
-
-from photo.models import *;ps = Photo.objects.all();ps = Photo.objects.all()
