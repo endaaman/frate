346ae7b4df0c783acf3e66065d70477128be2870
endaaman <buhibuhidog@gmail.com>
2016-04-25 15:18:01 +0900

    gracefull reloadにちゃんと対応

--- run.sh
+++ run.sh
@@ -1,25 +1,17 @@
+#!/bin/bash
+
+running_container_id=`echo $(docker ps -qa --no-trunc -f 'ancestor=frate' -f "status=running") | sed -e "s/[\r\n]\+/ /g"`
+
 docker build -t frate .
+docker run -d \
+  -e VIRTUAL_HOST=frate-alpine.club,frate.endaaman.me,frate.local \
+  -v $(pwd)/media:/var/www/frate/media \
+  -v $(pwd)/db:/var/www/frate/db \
+  frate
 
-# docker run -d -e VIRTUAL_HOST=frate-alpine.club \
-#   -v $(pwd)/media:/var/www/frate/media \
-#   -v $(pwd)/db:/var/www/frate/db \
-#    endaaman/frate
-
-running_container_id_raw=$(docker ps -aq --no-trunc --filter='ID=frate')
-running_container_id=`echo $running_container_id_raw | sed -e "s/[\r\n]\+//g"`
-#
-# echo $running_container_id
-
-container_id=$( \
-  docker run -d \
-    --label ID="frate" \
-    -e VIRTUAL_HOST=frate-alpine.club,frate.endaaman.me,frate.local \
-    -v $(pwd)/media:/var/www/frate/media \
-    -v $(pwd)/db:/var/www/frate/db \
-    frate \
-)
-
-if ! [ -n "$running_container_id" ]; then
-  echo 'killing old container'
+if [ -n "$running_container_id" ]; then
+  echo 'killing old running container'
   docker kill -s QUIT $running_container_id
+else
+  echo 'There is no old container'
 fi
